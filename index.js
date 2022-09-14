08de29bfbaaa851e3989749831b25ce760d5e250
Nimrod <87605179+Panther-12@users.noreply.github.com>
2022-09-14 01:49:24 +0000

    date to timestamp route

--- index.js
+++ index.js
@@ -24,6 +24,58 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/", (req, res)=>{
+  var utc_date = new Date();
+  let unix_time = utc_date.getTime()
+  res.json({
+    utc:utc_date.toUTCString(),
+    unix:unix_time
+  })
+})
+
+// convert timestamp to UTC time
+//app.get("/api/:utc", (req, res)=>{
+//  utc_date = new Date(req.params.utc);
+//  res.json({
+//    utc: utc_date.toUTCString()
+//  })
+//})
+
+// convert date to unix timestamp (january 1, 1970)
+app.get("/api/:date", (req, res)=>{
+  var date_string = req.params.date;
+  // Determine whether the variable is a date or timestamp
+  const char1, char2 = '-', '/';
+  if(date_string.includes(char) || date_string.includes(char)){
+    
+  }
+  else{
+    var utc_date = new Date(req.params.date);
+    res.json({
+      utc: utc_date.toUTCString()
+    })
+  }
+  
+  // Append the Z to get the recommended UTC Time
+  if(!date_string ){
+    var utc_date = Date.now("Z");
+    console.log(utc_date);
+  }
+  else if(date_string === new Date(date_string).toString()){
+    var utc_date = new Date(date_string+"Z");
+  }
+  else{
+    res.json({
+      error: "Invalid date"
+    })
+  }
+  var unix_time = utc_date.getUnixTime();
+  res.json({
+    utc: utc_date,
+    unix: unix_time
+  })
+})
+
 
 
 // listen for requests :)
